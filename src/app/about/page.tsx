74061fa0381da33fce86771ce51124af0ce0ffc5
PappySunseed <calebruby.cs@gmail.com>
2025-09-26 12:49:32 -0400

    added dummy pages for about, resume, contact

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main className="flex-grow flex items-center justify-center">
+      <h1 className="text-6xl font-bold">ABOUT</h1>
+    </main>
+  );
+}
